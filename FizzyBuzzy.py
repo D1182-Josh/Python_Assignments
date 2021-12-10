f65e6b1d9bd63e539ffd6b06968958b44c8efbad
coskun duman <dumanc753@gmail.com>
2021-12-11 02:43:16 +0300

    question

--- FizzyBuzzy.py
+++ FizzyBuzzy.py
@@ -1,3 +1,12 @@
+#Print numbers from 1 to 100 inclusively following these instructions:
+
+#1.if a number is multiple of 3, print "Fizz" instead of this number,
+#2.if a number is multiple of 5, print "Buzz" instead of this number,
+#3.for numbers that are multiples of both 3 and 5, print "FizzBuzz",
+#4.print the rest of the numbers unchanged.
+
+
+
 for i in range(1, 101):
       if i % 3 == 0:
    print("Fizzy")
