c67272a77242296eac8ce7bdf21507f253de3a5d
coskun duman <dumanc753@gmail.com>
2021-12-20 13:55:42 +0300

    code was written in to sorted_reversed.py file

---
+++ sorted_reverse.py
@@ -0,0 +1,20 @@
+    #TASK: 
+    # Write python code that writes in descending order from 1 to 10
+
+
+
+
+
+       SOLUTION1:
+
+        sıra = []
+        for i in range(0, 11):
+          sıra.append(i)
+        sıra = sorted(sıra, reverse=True)
+        sıra
+
+
+        SOLUTION2:
+        
+        liste = sorted([i for i in range(1,11)], reverse = True)
+        liste
