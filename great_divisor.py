2e20afaa4859c1d6c0e65bc8c7085ebfba2ad699
coskun duman <dumanc753@gmail.com>
2021-12-27 13:12:47 +0300

    task's solution  was written in it

--- great_divisor.py
+++ great_divisor.py
@@ -2,3 +2,16 @@
     Write a python programme to compute the greatest common divisor(GCD) of two pozitive integers
 
 
+    #SOLUTİON
+
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter a number: "))
+
+if number1 > number2 :
+  bolen = number1
+else :
+  bolen = number1
+for i in range(1, bolen+1):
+  if number1 % i == 0 and number2 % i ==0 :
+   great_divisor = i
+print(great_divisor)   
