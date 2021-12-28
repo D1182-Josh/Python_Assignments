e22c0837eb0c236c8899a7c9216e12d1c313aa94
coskun duman <dumanc753@gmail.com>
2021-12-28 14:15:35 +0300

    example  was written in the file

--- repeated_word.py
+++ repeated_word.py
@@ -1,3 +1,9 @@
     #TASK#
 
  #   Write a program to find how many time substring " word" appears in the given string  
+Example: str_x : "Emma is good developer. Emma is a writer"
+output:  Emma appeared 2 times
+
+ #SOLUTION
+
+    
