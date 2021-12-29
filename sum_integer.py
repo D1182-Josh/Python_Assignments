2e9276591b02338eddec3ebebef505a772aaab80
coskun duman <dumanc753@gmail.com>
2021-12-29 11:02:22 +0300

    solution was written in the sum_integer file

--- sum_integer.py
+++ sum_integer.py
@@ -4,7 +4,11 @@
 
     #SOLUTION
 
-    
+    num =list(map(int, input("").split()))
+    if num[0] == num[1] or num[1] == num[2] or num[0] == num[2] :
+        print(0)
+    else:
+        print(sum(num))
     
 
 
