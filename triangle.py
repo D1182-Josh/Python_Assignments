6f201e7ff91fbeaf9f1bfeb0c14643ed3caa9193
coskun duman <dumanc753@gmail.com>
2021-12-30 11:17:38 +0300

    task was written in the triangle file

---
+++ triangle.py
@@ -0,0 +1,8 @@
+    #TASK
+
+#    if the entered side lengths do not form a triangle, write the code that determines the type of triangle according to the three side information entered by the user.(In absolute value, the difference of two sides is taken, if the difference is greater than the other side, the entered values do not form a triangle. This is done for all sides.)
+
+
+    #SOLUTION
+
+
