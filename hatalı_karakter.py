851f81b2727eaa414bf9d9b6af77911c8a21e626
coskun duman <dumanc753@gmail.com>
2021-12-31 11:27:06 +0300

    solution was added in the file

--- hatalı_karakter.py
+++ hatalı_karakter.py
@@ -3,4 +3,15 @@
 # Kullanıcının girdiği hatalı karakterleri kalın metin(rakam veya özel karakterleri) temizleyen ve sonuçta "Ali Demir" gibi çıktı veren bir kod yazınız.
 
 
+   #SOLUTION
 
+s = input("sentence")
+new_s =""
+
+for i in s :
+  if i.isalpha() or i == " " :
+    new_s += i
+
+
+capitalize_s = " ".join([j.capitalize() for j in new_s.split()])
+capitalize_s   
