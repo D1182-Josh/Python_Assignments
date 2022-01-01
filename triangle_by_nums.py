9da56c1110f5a4a170f56384ee8859d331dff535
coskun duman <dumanc753@gmail.com>
2022-01-01 22:14:29 +0300

    solution4 was written in the file

--- triangle_by_nums.py
+++ triangle_by_nums.py
@@ -37,5 +37,14 @@ for i in range(num) :
 
   #SOLUTION4
 
-  
+#      1 
+#     1 2 
+#    1 2 3 
+#   1 2 3 4 
+#  1 2 3 4 5
+num = int(input(""))
+for i in range(1,num+1) :
+  print(" " * (num +1 -i), *range(1,i+1))  
+
+
 
