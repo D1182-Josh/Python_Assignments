fd0c86accd645917724c5218393c69176b2a9202
coskun duman <dumanc753@gmail.com>
2022-01-01 22:08:04 +0300

    solution2 was written in the file

--- triangle_by_nums.py
+++ triangle_by_nums.py
@@ -11,3 +11,23 @@
 # *
 for i in range(5,0,-1):
   print(i * "*")
+
+  #SOLUTION2
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+
+num = 5
+for i in range(num) :
+  for j in range(1, i+1):
+    print(j, end = " ")
+  print()
+
+
+
+  #SOLUTION3
+
+
+
