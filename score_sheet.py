a2cec3579d898e6965ad07d71e404d175747baf8
coskun duman <dumanc753@gmail.com>
2022-01-03 11:07:21 +0300

    task was written in the file

---
+++ score_sheet.py
@@ -0,0 +1,7 @@
+   #TASK
+
+#Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given scores. score them in a list and find the score of the runner-up. Input Format the first line contains. The second line contains an array of integers each seperated by a space.
+
+    #SOLUTION
+
+
