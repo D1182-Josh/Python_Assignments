c58444f853b973e50af159422d4dc1a3e0a24ec3
coskun duman <dumanc753@gmail.com>
2022-01-03 12:13:39 +0300

    solution was written in the exam_schedule.py file

--- exam_schedule.py
+++ exam_schedule.py
@@ -4,4 +4,8 @@
 
     #SOLUTION
 
+# exam_st_date = (11, 12, 2014)
+# Sample Output : The examination will start from : 11 / 12 / 2014
 
+exam_st_date = (11,12,2014)
+print("The examination will start from : %i / %i / %i "%exam_st_date)
