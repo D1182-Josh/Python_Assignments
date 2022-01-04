749e77b175555d48833261ec31a761f396542aa9
coskun duman <dumanc753@gmail.com>
2022-01-04 22:20:26 +0300

    Solution was written in the ekok.py file

--- ekok.py
+++ ekok.py
@@ -5,4 +5,9 @@
 
     #SOLUTION
 
-
+sayi1 =int(input("lütfen ilk sayıyı giriniz : "))
+sayi2 =int(input("lütfen ikinci sayıyı giriniz : "))
+for i in range(1,min(sayi1, sayi2) +1):
+    if (sayi1 % i==0) and (sayi2%i ==0):
+        ebob = i
+print("Ekok = ", (sayi1*sayi2)//ebob)
