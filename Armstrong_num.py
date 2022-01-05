024014d4df415dcf22e33d87d79753579c839cf9
coskun duman <dumanc753@gmail.com>
2022-01-05 16:12:11 +0300

    task was written in the file

---
+++ Armstrong_num.py
@@ -0,0 +1,7 @@
+    #TASK
+
+#A number from the user will be set to be "Armstrong". For example, if a number has 4 digits and is equal to the 4th (3rd power for 3-digit number) of each of its surroundings, this number is called the "Armstrong" number.
+
+    #SOLUTION
+
+
