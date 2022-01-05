e83d021363aa2e056a0d2660567356db3048915b
coskun duman <dumanc753@gmail.com>
2022-01-05 16:19:15 +0300

    solution was written in the find_armstrong.py file

--- find_armstrong.py
+++ find_armstrong.py
@@ -4,4 +4,15 @@
 
     #SOLUTION
 
-    
+# let’s find a similar one to this question, friends.
+# write a program to find all the Armstrong numbers present in between two intervals in Python. For example, Armstrong numbers between 100 and 2000. Expected output:
+# 153
+# 370
+# 371
+# 407
+# 1634
+
+lower_num, higher_num = list(map(int, input("").split()))
+for i in range(lower_num, higher_num+1) :
+  if sum([int(j) ** len(str(i)) for j in str(i)]) == i :
+    print(i)    
