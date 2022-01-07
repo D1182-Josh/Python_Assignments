81d959599a3d050818d153c4b39011fc26617ed9
coskun duman <dumanc753@gmail.com>
2022-01-07 16:04:29 +0300

    Task was written in the file

---
+++ FizzBuzz.py
@@ -0,0 +1,3 @@
+    #TASK
+
+#Write a Python program which iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of number and for the multiplesof of five print " Buzz". For numbers which are multiples of both three and five print "FizzBuzz". Sample Output: fizzbuzz 1 2 fizz 4 buzz.    
