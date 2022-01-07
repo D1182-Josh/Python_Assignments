11435aae7c780fdee3e7a1fa75ddc42e9db21cf9
coskun duman <dumanc753@gmail.com>
2022-01-07 16:09:25 +0300

    task was written in the file

---
+++ sum_q.py
@@ -0,0 +1,7 @@
+    #TASK
+
+#in each while loop, take a number from the user and the numbers entered by the user to a variable named "sum". When the user presses the "q" key, terminate the loop and print the "sum variable" to the screen.
+
+    #SOLUTION
+
+    
