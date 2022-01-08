5ba0aef6a50a5a2bb15227ea6b42f1ba84432e33
coskun duman <dumanc753@gmail.com>
2022-01-08 21:39:35 +0300

    task was written in it

---
+++ missing_num.py
@@ -0,0 +1,8 @@
+    #TASK
+
+#Ajumpbled list is given whose elements are consecutive numbers.Write python code that outputs even numbers of missing numbers that are between the smallest element and the largest element of the list,but not shown in the list.
+
+
+    #SOLUTION1
+
+    
