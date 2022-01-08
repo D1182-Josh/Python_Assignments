40a1bae49cf1d360d7e0bee4de03a040722cb310
coskun duman <dumanc753@gmail.com>
2022-01-08 21:45:07 +0300

    solution1  was written in it

--- missing_num.py
+++ missing_num.py
@@ -14,4 +14,10 @@ Expected Output = 8 12 14 32 38 40
 
     #SOLUTION
 
-    
+liste = [48, 10, 11, 21, 36, 5, 6, 52, 28, 29, 53, 54, 45, 19, 20, 47, 55, 39, 41, 7, 9, 17, 26, 27, 42, 22, 37, 51, 46, 18, 44, 30, 34, 13, 15, 35, 33, 16, 50, 24]
+ 
+missing_nums = [i for i in range(min(liste), max(liste)+1) if i % 2 == 0 and not i in liste]
+print(* missing_numsliste = [48, 10, 11, 21, 36, 5, 6, 52, 28, 29, 53, 54, 45, 19, 20, 47, 55, 39, 41, 7, 9, 17, 26, 27, 42, 22, 37, 51, 46, 18, 44, 30, 34, 13, 15, 35, 33, 16, 50, 24]
+         
+        missing_nums = [i for i in range(min(liste), max(liste)+1) if i % 2 == 0 and not i in liste]
+        print(* missing_nums))    
