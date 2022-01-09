884a9253f06bec4a29479e801ad6cade36e1e8b4
coskun duman <dumanc753@gmail.com>
2022-01-09 21:23:13 +0300

    solution was written in the cube_of_integer.py file

--- cube_of_integer.py
+++ cube_of_integer.py
@@ -3,3 +3,6 @@
 #Write a python function that takes a positive integer and returns the sum of the cube of all the positive integers smoller than the specified number.
 
     #SOLUTION
+
+num = int(input(""))
+print(sum([i ** 3 for i in range(num)]))    
