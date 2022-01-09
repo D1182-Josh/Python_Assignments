2751149bb46a1cbfeb8a239edfd64e3005681db4
coskun duman <dumanc753@gmail.com>
2022-01-09 21:18:22 +0300

    task was written

---
+++ index_string.py
@@ -0,0 +1,12 @@
+    #TASK
+
+#Write a Python dictionary function that takes  a string from the user and matches each character in the string with the index number.
+
+#For Example
+input: 'clarusway'
+output : {0:'c', 1:'l', 2:'a', 3:'r', 4:'u', 5:'s', 6:'w', 7:'a', 8:'y'}
+
+
+    #SOLUTION
+ 
+
