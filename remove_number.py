3de8647adf1bf4ebfa21152721ccd870e0855ca7
coskun duman <dumanc753@gmail.com>
2022-01-10 19:31:49 +0300

    solution was written in the file

--- remove_number.py
+++ remove_number.py
@@ -3,3 +3,13 @@
 #Write a python program to remove and print every third number from a list of numbers until the list becomes empty.
 
     #SOLUTION
+
+liste = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9]
+indeks = 0
+uzunluk = len(liste)
+while len(liste) > 0 :
+  indeks = (2 + indeks) % uzunluk
+  liste.pop(indeks)
+  print(liste)
+  uzunluk -= 1
+
