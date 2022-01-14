375ee5f8a3331fbc2e7f184206d8c0872a8c78f0
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-14 13:06:41 +0300

    solution  was written in the file

--- read_number.py
+++ read_number.py
@@ -6,4 +6,13 @@ For example: input : 97
 
     #SOLUTION
 
-    
+sayı = int(input(""))
+birler= ["", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz"]
+onlar= ["", "On", "Yirmi", "Otuz", "Kırk", "Elli", "Altmış", "Yetmiş", "Seksen", "Doksan"]
+
+birinci_b = sayı % 10
+ikinci_b = sayı // 10
+if len(str(sayı)) == 2:
+  print(onlar[ikinci_b], birler[birinci_b])
+elif len(str(sayı)) == 1 :
+  print(birler[birinci_b])    
