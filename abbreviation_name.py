f30304406516b27488c419aa9b595ebf69488b3b
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-14 13:00:47 +0300

    task was writtten in the file

---
+++ abbreviation_name.py
@@ -0,0 +1,7 @@
+    #TASK
+
+#Write a program that takes your full name as input and displays the abbreviations of the first and middle names except the last name which is displayed as it is. For example, if name is Robert Roser, then the output should be R.B. Roser.
+
+    #SOLUTION
+
+
