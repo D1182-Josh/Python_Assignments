8c57a44bd353d9d3dbed86b7cfe48a02eb5baee1
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-14 13:06:10 +0300

    Task was written in the file

---
+++ read_number.py
@@ -0,0 +1,9 @@
+    #TASK
+
+#Write a program that takes a maximum rwo-digit(1-99) number from the user and finds the pronunciation of that number. 
+For example: input : 97
+             output : Doksan Yedi
+
+    #SOLUTION
+
+    
