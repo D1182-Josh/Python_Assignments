caeef01b4905395ec810083dc18a2824a0830af1
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-15 13:18:55 +0300

    solution 2 added

--- find_pronuniation.py
+++ find_pronuniation.py
@@ -4,7 +4,7 @@
 For example: input : 97
              output : Doksan Yedi
 
-    #SOLUTION
+    #SOLUTION1
 
 sayı = int(input(""))
 birler= ["", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", "Dokuz"]
@@ -15,4 +15,21 @@ ikinci_b = sayı // 10
 if len(str(sayı)) == 2:
   print(onlar[ikinci_b], birler[birinci_b])
 elif len(str(sayı)) == 1 :
-  print(birler[birinci_b])    
+  print(birler[birinci_b])
+
+ #SOLUTION2
+
+num = int(input("Enter your 2-digit number: "))
+ones = ["sıfır", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"]
+tens = ["", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"]
+digit_ones = num % 10
+digit_tens = num // 10
+print(tens[digit_tens], ones[digit_ones])    
+
+
+
+
+
+
+
+
