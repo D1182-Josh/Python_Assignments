0236e6c095b7addc2a556ea134ba14fcaf51c94b
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-15 15:04:15 +0300

    solution was written in the pythagorean_triangle.py file

--- pythagorean_triangle.py
+++ pythagorean_triangle.py
@@ -4,4 +4,8 @@ Write a program that prints the numbers from 1 to 100 a pythagorean triangle. Fo
 
     #SOLUTION
 
-    
+for x in range(1,100) :
+  for y in range(x,100) :
+    for z in range(y,100) :
+      if x ** 2 + y **2 == z ** 2 :
+        print(x,y,z)    
