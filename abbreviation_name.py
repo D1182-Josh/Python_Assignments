8f9e35c3ec7901806a848debc572dc374bab8f80
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-15 14:58:42 +0300

    varies solutions were added.

--- abbreviation_name.py
+++ abbreviation_name.py
@@ -6,3 +6,31 @@
 
 name = list(map(str.title, input("").split()))
 print("{}.{}.{}".format(name[0][0],name[1][0],name[2]))
+
+    #SOLUTION2
+
+name = list(map(str.upper, input("").split()))
+count = 0
+for i in name :
+  count += 1
+  if count < len(name) :
+    i = i[0] +( "*" * (len(i) -1))
+  else :
+    i = i
+  print(i , end = " ")
+
+    #SOLUTION3
+
+name = input("").split()
+short_name = ""
+for i in range(len(name) -1) :
+  short_name += name[i][0] + len(name[i][1:]) * "*" + " "
+short_name += name[-1]
+print(short_name.upper())
+
+
+
+
+
+
+
