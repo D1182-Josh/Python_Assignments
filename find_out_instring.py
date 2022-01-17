181a4ebe7b8379b4c55345c4b2db1ea1080b9ab4
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-18 02:58:20 +0300

    task was written in the file

---
+++ find_out_instring.py
@@ -0,0 +1,21 @@
+    #TASK
+
+#You are given a string. Your task is find out if the string contains: alphanumeric characters, digits, lowercase and uppercase characters.
+
+#ınput format 
+#A single line containing a string.
+
+#output format 
+
+#in the first line, print True if has any alphanumeric characters. Otherwise, print False. In the second line, print True if has any alphabetical characters. Otherwisw, print False. ın the third line, print True if has any digits. Otherwise , print False. In the fourth line, prin True if has any lowercase. Otherwise, print False. In the fifth line, print True if has any uppercase characters. Otherwise, print False.
+
+Sample input
+qA2
+
+Sample output 
+True
+True
+True
+True
+True
+
