a1381d46351d65d7f002d53475745aa0e6619814
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-18 02:59:32 +0300

    solution was written in the file

--- find_out_instring.py
+++ find_out_instring.py
@@ -7,6 +7,7 @@
 
 #output format 
 
+
 #in the first line, print True if has any alphanumeric characters. Otherwise, print False. In the second line, print True if has any alphabetical characters. Otherwisw, print False. ın the third line, print True if has any digits. Otherwise , print False. In the fourth line, prin True if has any lowercase. Otherwise, print False. In the fifth line, print True if has any uppercase characters. Otherwise, print False.
 
 Sample input
@@ -19,3 +20,12 @@ True
 True
 True
 
+
+    #SOLUTION
+text = input("")
+print(any(i.isalnum() for i in text))
+print(any(i.isalpha() for i in text))
+print(any(i.isdigit() for i in text))
+print(any(i.islower() for i in text))
+print(any(i.isupper() for i in text))
+    
