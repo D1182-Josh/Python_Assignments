b3fd52f16ed8a4ce8716c7d169acdcf5f804abf5
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-19 21:23:52 +0300

    task was written in the file

---
+++ harshad.py
@@ -0,0 +1,17 @@
+    #TASK   
+
+#Anumber is said to be Harshad if it's exactly divisible by the sum of its digits. Create a code that determines whether a number is a harshad or not.
+INPUT:75 --> OUTPUT:False
+
+7 + 5 = 12
+
+75 is not exactly divisible by 12
+
+INPUT :171 --> OUTPUT:True 
+
+1 + 7 + 1 = 9
+9 exactly divides 171
+
+    #SOLUTION
+
+    
