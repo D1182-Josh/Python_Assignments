c4666edb9fefde54d1aa8922aea8a0ead734d7f0
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-19 21:09:23 +0300

    solution was written in the file cuboid.py

--- cuboid.py
+++ cuboid.py
@@ -13,4 +13,12 @@ Print an array of the elements that do not sum to 2.
 
     #SOLUTION
 
+x, y, z, n = int(input("")), int(input("")), int(input("")), int(input(""))
+ls = []
+for i in range(x +1) :
+  for j in range(y+ 1) :
+    for k in range(z + 1) :
+      if i + j + k != n :
+        ls.append([i,j,k])
+print(ls)
     
