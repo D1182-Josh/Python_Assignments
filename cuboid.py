d23a295fb6e6dc99afe79dec0657be35ee5604ab
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-19 21:08:43 +0300

    task was written in the file cuboid.py

---
+++ cuboid.py
@@ -0,0 +1,16 @@
+    #TASK
+
+#you are given three integers i, j, and k representing the dimension of a cuboin along with an integer n. Print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum i + j + k of is not equal to n.
+
+Sample Input 0
+1 
+1
+1
+2
+Sample Output 0
+[[0,0,0],[0,0,1],[0,1,0],[1,0,0],[1,1,1]]
+Print an array of the elements that do not sum to 2.
+
+    #SOLUTION
+
+    
