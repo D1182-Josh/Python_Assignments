1edff54108922d3781894e1d9e6d5982e32a841c
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-20 21:21:54 +0300

    task was written in the file

---
+++ merged_list.py
@@ -0,0 +1,15 @@
+    #TASK
+
+#Write a Python to create a merged list of tuples from the two listd given.
+For Example : 
+    input:
+        list1=[195,488,142,626,180]
+        list2=["New York","Alabama","Hawaii","Vermont","West Virginia"]
+
+     output:
+
+         list3=[(195,'New York'),(488,'Alabama'),(142,'Hawaii'),(626,'Vermont'),(180,'West Virginia')]
+
+   #SOLUTION
+
+   
