1cd5096670db2df2eb4734f8600dca858da0fbad
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-22 11:40:57 +0300

    solution   was  written in the file

--- loves_me.py
+++ loves_me.py
@@ -5,3 +5,20 @@
 #    INPUT : 3 ---> "Loves me, Loves me no, LOVES ME"
 #    INPUT : 6 ---> "Loves me,Loves me not, Loves me, Loves me not, Loves me, LOVES ME NOT"
 #    ınput : 1 ---> "LOVES ME"
+
+    #SOLUTION
+n = int(input(""))
+papatya = ["Loves me", "Loves me not" ]
+yeni_papatya = []
+if n == 1 :
+  print(papatya[0].upper())
+else:
+  for i in range(1,n + 1) :
+    if i % 2 == 1 :
+      yeni_papatya += [papatya[0]]
+    elif i % 2 == 0 :
+      yeni_papatya += [papatya[1]]
+  yeni_papatya  = yeni_papatya[:-1] + [yeni_papatya[-1].upper()]
+  print(", ".join(yeni_papatya))
+
+  
