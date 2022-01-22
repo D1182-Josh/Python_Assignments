650ea0cc8d0596f7ba78b8df25f373f6f9d02aa4
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-22 11:33:54 +0300

    task was  written in the file

---
+++ loves_me.py
@@ -0,0 +1,3 @@
+    #TASK
+
+#"Loves me, loves me not " is a traditional game in which a person plucks off all the pedals of a flower one by one , saying the phrase "Loves me" and "Loves me not" when determining whether the one that love, loves them back. Given a number of pedals, return a string which repeats the phrases "Loves me" and "Loves me not" for every alternating pedaş, and return the last phrase in all caps. Remember to put a momma and space between phrases.
