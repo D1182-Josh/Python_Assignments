d3e474550463bf286d08236f97db979725229c27
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-23 19:26:47 +0300

    task was  written in the same_first_element.py file

---
+++ same_first_element.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#Given 2 arrays of ints, a and b, return True if they have the same first element or they have the same last element. Both arrays will be length 1 or more . (without using for, if , while)
+#  
+#   [1,2,3],[7,3] -->  True
+#   [1,2,3],[7,3,2]--> False
+#   [1,2,3],[1,3] --> True
+
+    #SOLUTION
+
+    
