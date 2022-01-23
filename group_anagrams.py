cffe2a2dae83832148e55f30e60e50b0bda53641
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-23 19:44:03 +0300

    task was written in the group_anagrams.py file

---
+++ group_anagrams.py
@@ -0,0 +1,19 @@
+    #TASK
+
+#Title: given an array of string,you can combine words with different letters. Alphabetic words refer to strings with the same letters but arranged differently. Given an array of string, group anagrams together.
+#Example:
+#input:["eat","tea","tan","ate","nat","bat"]
+#output:
+    **[
+    ["ate","eat","tea"],
+    ["nat","tan"],
+    ["bat"]
+     ]
+#Explain:
+    #All inputs are lowercase.Do not consider the order of the answer output.Note:
+#All inputs will be in lowercase.The order your output does not matter.
+
+    #SOLUTION
+
+    
+
