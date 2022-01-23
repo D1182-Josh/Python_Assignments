cb6f03e18070ed53849e91629c8d412ab9f0e14b
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-23 19:44:40 +0300

    solution  was written in the group_anagrams.py file

--- group_anagrams.py
+++ group_anagrams.py
@@ -15,5 +15,15 @@
 
     #SOLUTION
 
-    
+strs = ["eat", "tea", "tan", "ate", "nat", "bat", "cat", "tac"]
+group = {}
+
+for i in strs :
+  kelime = "".join(sorted(i))
+  if kelime in group :
+    group[kelime].append(i)
+  else :
+    group[kelime] = [i]
+
+print(list(group.values()))    
 
