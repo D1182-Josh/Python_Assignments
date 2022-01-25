e9c9618d9da5cd94334f2a95919c3a225661eb96
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-26 01:40:24 +0300

    task was written in the file

---
+++ toys_price.py
@@ -0,0 +1,24 @@
+    #TASK
+
+#Mark and Jane are very happy after having their first child. Their son loves toys, so mark wants to buy some. There are a number of different toys lying in front of him, tagged with their prices. Mark has only a certain amount to spend, and he wants to maximize the number of toys he buys with this money.Given a list of toy prices and an amount tı spend, determine the maximum number of gifts he can buy.Note each toy can be purchased only once.
+
+#Example:
+ #The budget is units of currency. He can buy items that cost for or for units.The maximum is items.
+ #Function Description
+ #Complete the function maximum Toys in the editor below.
+ #maximum Toys has the following parameters:
+ #int prices[n]:the toy prices intk:Mark's budget Returns
+ #int:the maximum number of toys input Format
+ #The first line contains two integer and the number of priced toys and amount Mark has to spend. The next line contains space-separated integrs
+ #Constraints
+ #A toy can't be bought multiple times.
+ #Sample Input
+ 7 50
+ 1 12 5 111 200 1000 10
+ #Sample Output 
+ 4 Explanation
+#He can buy only toys at most.These have the following prices.
+
+    #SOLUTION
+
+    
