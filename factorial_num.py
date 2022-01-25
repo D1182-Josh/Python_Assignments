4715d9016c7d60c5b17053d1144a3834bd87dcb3
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-26 01:20:40 +0300

    task was written in the file

---
+++ factorial_num.py
@@ -0,0 +1,8 @@
+    #TASK
+
+#Pyhton program to find the factorial of a number is the product of all the integers from 1 to that number.For example, the factorial of 6 is 12345*6=720. Factorial is not defind for negative numbers, and the factorial of zero is one , 0!=1
+
+
+    #SOLUTION
+
+    
