e3c1f45e41edec49f0d8d1fe07ad01d60ff00418
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-27 22:14:54 +0300

    task was written in the file

---
+++ balanced_strings.py
@@ -0,0 +1,14 @@
+    #TASK
+
+#Write a program to check if two strings are balanced. For example, strings s1 and s2 are balanced if all the characters in the s1 are present in s2. The character's position doesn't matter. 
+
+ Given:
+     Case 1:                             Case 2:
+         s1 = "Yn"                            s1 = "Ynf"
+         s2 = "PYnative"                      s2 = "PYnative"
+     Ecpected Output :                   Expected Output:
+         True                                 False
+
+    #SOLUTION
+
+
