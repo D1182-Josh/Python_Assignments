01a3bb27cbf7da1f0cc30dee60f1e267bb821383
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-27 22:22:38 +0300

    solutions were written in the file

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -10,6 +10,24 @@ output :
     [1, 4, 6, 4, 1]
     [1, 5, 10, 10, 5, 1]
 
-    #SOLUTION
+    #SOLUTION1
+
+n = int(input(""))
+for i in range(n) :
+  print(list(str(11 ** i)))
+
+
+    #SOLUTION2
+
+pascal =[[1],[1,1]]
+for i in range(2,10):
+  liste=[1]
+  for j in range(1,i):
+    liste.append(pascal[i-1][j-1]+pascal[i-1][j])
+  liste.append(1)
+  pascal.append(liste)
+for i in pascal:
+  print(i) 
+    
 
 
