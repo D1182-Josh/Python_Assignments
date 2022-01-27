62371d93ab59c3644a44024e7aa89bf31d3318f2
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-27 22:20:58 +0300

    task was written in the file

---
+++ pascals_triangle.py
@@ -0,0 +1,15 @@
+    #TASK
+  
+#Write python codes that calculate the digits of pascal's triangle as many as the number of digits received from the user. 
+# input : 6
+output : 
+    [1]
+    [1, 1]
+    [1, 2, 1]
+    [1, 3, 3, 1]
+    [1, 4, 6, 4, 1]
+    [1, 5, 10, 10, 5, 1]
+
+    #SOLUTION
+
+
