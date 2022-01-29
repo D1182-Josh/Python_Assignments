a0e2ad2e95ac0ea248ef5affbfcd862c8ea47428
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-30 01:37:25 +0300

    solution  was written in the file

--- prime_num_10000.py
+++ prime_num_10000.py
@@ -4,4 +4,13 @@
 
     #SOLUTION
 
+asal_sayılar = [1,2]
+num = 3
+while len(asal_sayılar)<10000:
+    for i in range(2,num):
+        if num%i==0:
+            break
+    else:
+        asal_sayılar.append(num)
+    num+=1
     
