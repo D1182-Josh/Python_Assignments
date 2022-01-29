c2c9c03780223111b0e0f231c43c01f1cdef87f4
Coşkun DUMAN <dumanc753@gmail.com>
2022-01-29 19:58:59 +0300

    task was written in the file

---
+++ prime_num.py
@@ -0,0 +1,14 @@
+    #TASK
+
+#Given a number N, the task is to print the prime numbers from 1 to N.
+# Example:    Input : N = 10
+		      Output: 2, 3, 5, 7
+
+#Example:     Input: N = 5
+              Output: 2, 3, 5
+
+#A prime number is a natural number greater than 1, which is only divisible by 1 and itself.
+
+    #SOLUTION
+
+    
