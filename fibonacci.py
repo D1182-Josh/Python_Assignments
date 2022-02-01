6c45e606ec10f90118db7a1bbf4adf9abac855e0
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-01 18:20:13 +0300

    task was written in the file

---
+++ fibonacci.py
@@ -0,0 +1,7 @@
+    #TASK
+
+#The Fibonacci sequence of number is a sequence of numbers whose first two terms are 1 and each subsequent term is the sum of the two preceding terms. Print the first 100 fibonacci numbers to the screen.
+
+    #SOLUTION
+
+    
