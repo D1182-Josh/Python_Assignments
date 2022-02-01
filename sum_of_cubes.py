7c1ba2eeda8f2c2698e19c42bc7b58e2106f1217
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-01 18:10:04 +0300

    solution was written in the file

--- sum_of_cubes.py
+++ sum_of_cubes.py
@@ -4,4 +4,13 @@
 
     #SOLUTION
 
-    
+toplam = 0
+counter = 0
+for i in range(100, 1000) :
+  for j in str(i) :
+    toplam = toplam + int(j) ** 3
+  if i == toplam :
+    counter += 1
+    print(toplam)
+  toplam = 0
+print(counter)    
