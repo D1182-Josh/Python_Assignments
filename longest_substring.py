3ab74a8ec8a2786819853c546689909ae2bf7944
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-02 22:31:26 +0300

    task was written in the file

---
+++ longest_substring.py
@@ -0,0 +1,16 @@
+    #TASK
+
+#Given a string s, return the longest palindromic substring in s.
+
+#Example1 : input: s="babad"
+            Output: "bab"
+
+#Note: "aba" is also a valid answer.     
+
+#Example 2:  input: s="cbbd"
+             output: "bb"
+
+        
+        #SOLUTION
+
+        
