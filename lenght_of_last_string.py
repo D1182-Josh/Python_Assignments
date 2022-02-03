18cd5a6ab42bbc31050ad307321ef83c2b61c34e
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-03 22:28:21 +0300

    task was writen in the file

---
+++ lenght_of_last_string.py
@@ -0,0 +1,11 @@
+    #TASK 
+
+#Given a string s consisting of some words seperated by some number of spaces, return the lenght of the last word in the string. a word is a maximal substring consisting of non-space characters only.
+    example 
+        input: s = "Hello World"
+        output: 5
+     Explanation: The last word is "World" with length 5
+
+    #SOLUTION
+
+
