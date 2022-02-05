438655bdd6b4a13e9ca1e86b6b9f2bf635cdabe0
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-05 23:10:11 +0300

    task was written in the file

---
+++ replace_letter_position.py
@@ -0,0 +1,8 @@
+    #TASK
+
+#Write a program that given a string and replace every letter with its position in the alphabet. If anything in the text isn't a letter , ignore it and don't return. "a"=1, "b"=2, etc. Example alphabet_position("The sunset sets at twelve o'clock.") Should return "20 8 5 19 21 14 19 5 20 19 1 20 20 23 5 12 22 5 3 12 15 3 11" 
+
+
+    #SOLUTION
+
+    
