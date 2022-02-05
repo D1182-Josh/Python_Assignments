fba81112f618e510eeb3a654b28d35224dc492ed
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-05 23:04:41 +0300

    task was written in the file

---
+++ square_digits.py
@@ -0,0 +1,7 @@
+    #TASK
+
+#Write a program that square every digit of a number and concatenate them.For Example: if we run 9119 through the function, 811181 will come out, because 82 is 81 and 12 is 1.
+
+    #SOLUTION
+
+    
