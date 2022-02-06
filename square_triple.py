e36d4a08b24ccd2a3b86df26dcdbab94b2454508
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-06 18:01:37 +0300

    solution was written in the file

--- square_triple.py
+++ square_triple.py
@@ -8,4 +8,12 @@
 
      #SOLUTION
 
-     
+n = int(input(""))
+counter = 0
+
+for i in range(1,n+1) :
+  for j in range(1,n+1) :
+    for k in range(1,n+1) :
+      if i ** 2 + j ** 2 == k ** 2 :
+        counter += 1
+print(counter)     
