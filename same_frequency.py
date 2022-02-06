d7d6900ff57a627969e0c7629203091689f0ab01
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-06 17:48:48 +0300

    task was written in the file

---
+++ same_frequency.py
@@ -0,0 +1,12 @@
+    #TASK
+
+#Given a string s, return True if s is a googd string, or False otherwise. A string s is good if all the characters that appear in s have  the same number of occurrences.
+    Example :
+        Input: s = "abacbc"
+        Output: True 
+    Explanation: The characters that appear in s are 'a', 'b', and 'c'. All characters occur 2 times in s.
+
+
+    #SOLUTION
+
+    
