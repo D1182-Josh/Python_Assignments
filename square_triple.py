8b9e7abb842a0040e70b0554363d0f416df11c09
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-06 18:00:53 +0300

    task was written in the file

---
+++ square_triple.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#Asquare triple(a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2. Given an integer n, return the number of square triples such that 1 <=a, b, c<=n. Example:
+        Input: n=5
+        Output: 2
+        Explanation: The square triples are (3,4,5) and (4,3,5)
+
+
+     #SOLUTION
+
+     
