9c066bd7a2152e2c46038cc5d4caf52254b52b99
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-07 19:55:10 +0300

    task was written in the population_in_town file

---
+++ population_in_town.py
@@ -0,0 +1,7 @@
+    #TASK
+ 
+#in a small town the population is p0=1000 at the beginning if a year. The population regulary increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population grester or equal to p=1200 inhabitants? 
+
+    #SOLUTION
+
+    
