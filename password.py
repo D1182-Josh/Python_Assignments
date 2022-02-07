975ed77294905b8322476c948c2311a636abf859
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-07 19:48:20 +0300

    task was written in the password.py file

---
+++ password.py
@@ -0,0 +1,7 @@
+    #TASK
+
+#Let's make a user name and password to login an account. Firstly define the username and password then take inputs for this two argument from user. DEsign python program 3 possibilities that first one is True user name and password, second is wrong username input and third is wrong password input. Then finish with try again output 
+
+    #SOLUTION
+
+    
