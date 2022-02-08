b718ddf4994c1bb1ef76f1f3021151c943e421a6
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-08 23:17:36 +0300

    task was written in the file

---
+++ power_of_two.py
@@ -0,0 +1,13 @@
+    #TASK
+
+#Given an integer n, return true if it is a power of two. Otherwise, return false. An integer n is a power of two, if there exists an integer x such that n==2x.
+
+    Example:
+        Input: n=1
+        Output: true
+        Explanation: 2⁰=1
+
+
+   #SOLUTION
+
+   
