1080b65e68dde3d2232f2cc72a98ec8e104b2af7
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-08 22:15:22 +0300

    task was written in the file

---
+++ reversed_letter.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed. Strings passed in will consists of only letters and spaces. Spaces will be included only when more than one word is present. 
+    Example:
+        spinWords("Hey fellow warriors") => "Hey wollef sroirraw"
+        spinWords("This is a test") => "This is a test"
+        spinWords("This is another test") => "This is rehtona test"
+
+   #SOLUTION
+
+   
