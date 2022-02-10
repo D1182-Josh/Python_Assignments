c7a15e6634021ffdf5df32aa3636d6e5ac89cec1
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-10 15:56:16 +0300

    task was written in the file

---
+++ non_repeating_char.py
@@ -0,0 +1,16 @@
+    #TASK
+
+# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+    
+    Example 1:
+        Input: s = "leetcode"
+        Output: 0
+
+    Example 2:
+        Input: s =  "loveleetcode"
+        Output: 2
+
+
+     #SOLUTION
+
+     
