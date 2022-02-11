7f2e5f12424d44d99a43cfce49f73cb9083b572c
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-11 23:34:21 +0300

    task was written in the file

---
+++ camelcasing.py
@@ -0,0 +1,13 @@
+    #TASK
+
+#Complete the solution so that the function will break camel casing, using a space between words. 
+    Example:
+         "camelCasing" => "camel Casing"
+         "identifier" => "identifier"
+         "" => ""
+         "breakCamelCase" => "break Camel Case"
+
+
+     #SOLUTION
+
+     
