20563d1293b410a3c01e3aae97b0d3577568442a
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-12 11:39:05 +0300

    task was written in the file

---
+++ harmonius_array.py
@@ -0,0 +1,19 @@
+    #TASK
+
+#   We define a harmonious array as an arraywhere the difference between its maximum value and its minimum value is exactly 1. Given an integer array nums, return the length of its longest harmmonious subsequence among all its possible subsequences. A subsequence that can be derived from the array by deleting some or no elements without changing the order of the remaining elements. 
+
+    Example:
+
+        Input: nums=[1,3,2,2,5,2,3,7]
+        Output: 5
+        Explanation: The longest harmonious subsequence is [3,2,2,2,3].
+
+    Example:
+
+        Input: nums=[1,2,3,4]
+        Output: 2
+
+
+     #SOLUTION
+
+    
