493da0bd2a9a9596db2935fd4b46b2fd32d8e8fd
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-12 11:16:54 +0300

    task was written in the file

---
+++ squirrel_playing.py
@@ -0,0 +1,12 @@
+    #TASK
+
+#  The squirrels in Palo Alto spend mopst of the day playing. In particular, they play if the temprature is between 60 and 90 (inclusive). Unless it is summer, then the upper limits is 100 instead of 90. Given an int temprature and a boolean is_summer, return True if the squirrels play and False otherwise. 
+
+    (70, False) --> True
+    (95, False) --> False
+    (95, True) --> True
+
+  
+  #SOLUTION
+
+  
