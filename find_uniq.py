37e4a4d805f1cbe13149a560c4629dfc5a463bb0
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-13 23:56:20 +0300

    task was written in the file

---
+++ find_uniq.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#   There is an array with  some numbers. All numbers are equal except for one. Try to find it! 
+        find_uniq([1, 1, 1, 2, 1, 1]) == 2
+        find_uniq([0, , 0.55, 0, 0]) == 0.55
+
+
+     #SOLUTION
+
+
+     
