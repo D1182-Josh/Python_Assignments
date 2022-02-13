f8a017b4ee7bb16d72349b7f7d1dcad566115d74
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-13 23:56:42 +0300

    solution  was written in the file

--- find_uniq.py
+++ find_uniq.py
@@ -8,4 +8,9 @@
      #SOLUTION
 
 
-     
+def find_uniq(liste) :
+  for i in liste :
+    if liste.count(i) == 1 :
+      return i
+print(find_uniq([ 1, 1, 1, 2, 1, 1 ]))
+print(find_uniq([ 0, 0, 0.55, 0, 0 ]))     
