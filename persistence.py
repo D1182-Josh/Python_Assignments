d220e055770dda4392ae02113ef930acb4f993a4
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-14 19:47:41 +0300

    task was written in the file

---
+++ persistence.py
@@ -0,0 +1,17 @@
+    #TASK
+
+#   Write a program , persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit. 
+
+    persistence(39) # returns 3 , because 3*9=27, 2*7=14, 1*4=4
+                    #and 4 has only one digit
+
+    persistence(999) # returns 4, because 9*9*9=729, 7*2*9=126,
+                     # 1*2*6=12, and finaly 1*2=2
+            
+    persistence(4) # returns 0, because 4 is already a one-digit number
+
+
+
+    #SOLUTION
+
+    
