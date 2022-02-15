f2772d0f4426864a7ffd88a4f7c247cba94e37a6
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-15 23:34:13 +0300

    task was written in the file

---
+++ radius_of_circle.py
@@ -0,0 +1,12 @@
+    #TASK
+
+#   Imagine a circle and two squares: a smaller and a bigger one. For the smaller one, the circle is a circumcircle and for the bigger one, an incircle. Create a program, that takes an integer (radius of the circle) and returns the difference of the two squares. 
+
+    Examples
+        radius = 5 --> Output 50
+        radius = 6 --> Output 72
+        radius = 7 --> Output 98
+
+     #SOLUTION
+
+     
