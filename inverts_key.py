bf2b5c8717425ae8028718f1305c3742a4fc284a
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-15 23:39:00 +0300

    task was written in the file

---
+++ inverts_key.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#   Write a function that inverts the keys and values of a dictionary.
+    invert ={"zebra": "koala", "horse":"camel"}
+    Output
+      {"koala":"zebra","camel":"horse"}
+
+
+   #SOLUTION
+
+   
