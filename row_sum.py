68137aebf1860a7397d3e59398b7ce991bdeab8e
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-16 20:42:36 +0300

    task was written in the file

---
+++ row_sum.py
@@ -0,0 +1,16 @@
+    #TASK
+
+#Imagine this triangle:
+1
+2 3 
+4 5 6 
+7 8 9 10
+.........
+
+#Create a function that takes a number n and returns the sum of all numbers in nth row. Examples rowSum(1)--> 1
+        rowSum(2)--> 5
+        rowSum(4)--> 34
+
+     #SOLUTION
+
+     
