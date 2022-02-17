f4ef761e45700085c583697d3a1ed91ac4dffd3c
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-17 22:16:34 +0300

    task was written in yhe file

---
+++ unlimited_arguments.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#   Given a function that accepts unlimited arguments, check and count how many data types are in those arguments. Finally return the total in a list. List order is: [int, str, bool, list, tuple, dictionary]
+
+    Examples:
+        [1,45,"Hi", False] --> [2, 1, 1, 0, 0, 0]
+        [[10, 20,], ("t", "Ok"), 2, 3, 1] --> [3, 0, 0, 1, 1, 0]
+
+   #SOLUTION
+
+   
