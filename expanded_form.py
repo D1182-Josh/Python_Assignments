e230275904d48fe1a47e6e9b4edca315d9689886
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-17 22:24:47 +0300

    task was written in yhe file

---
+++ expanded_form.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#   Write number in Expanded Form you will be given a number and you will need return it as a string in . For Example: 
+                                12 >>>> '10 + 2'
+                                42 >>>> '40 + 2 '
+                                70304>>>> '70000 + 300 + 4'
+
+
+     #SOLUTION
+
+     
