028fde78637430e4a08bc704866f19483bab101a
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-17 22:25:45 +0300

    solution was written in yhe file

--- expanded_form.py
+++ expanded_form.py
@@ -6,6 +6,21 @@
                                 70304>>>> '70000 + 300 + 4'
 
 
-     #SOLUTION
+     #SOLUTION1
+
+num = input("")[::-1]
+" + ".join([str(int(i) * (10 ** num.index(i))) for i in num if i != "0"][::-1])
+
+    #SOLUTION2
+
+num = input("")[::-1]
+num_list = []
+for i in num :
+  if i == 0 :
+    
+  num_list += [str(int(i) *(10 **(num.index(i))))]
+
+" + ".join((num_list[::-1]))
+    
 
      
