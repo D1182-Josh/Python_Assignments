dc905ea2f96971edd03f48de6260457569b2580b
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-18 14:36:30 +0300

    task was written in the file

---
+++ title_case.py
@@ -0,0 +1,21 @@
+#   TASK
+
+#Astring is considered to be in title case if each word in string is either(a) capitalised(that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised. Write a function that will convert a string into title case, given an optinal list of exceptions (minor words). The list of minor words will be given as a string with each word seperated by a space. Yor function should ignore the case of the minor words string--it should behave in the same way even if the case of the minor word string is changed. First argument (required): the orginal string to be converted. Second argument(optinal): space-delimited list of minor words that must always be lowercase except for the first word in the string. 
+
+ #   Example
+
+    title_case('a clash of KING')
+    minor_words('a an the of')
+    Output: 'A Clash of King'
+
+
+    title_case('THE WIND IN THE WILLOWS'),
+    minor_words('The In')
+    Output: 'The Wind in the Willows'
+
+
+    #SOLUTION
+
+    
+
+
