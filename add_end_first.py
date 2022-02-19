f032f58dca7f41880874cddf91fc4fba9c21c7d8
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-19 23:02:38 +0300

    task was written in the file

---
+++ add_end_first.py
@@ -0,0 +1,11 @@
+#   TASK
+
+#   Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched. 
+    Examples:
+
+pig_it('Pig latin is cool') # igPay atinlay siay coolcay
+pi_it('Hello world !') # elloHay orldway !
+
+    #SOLUTION1
+
+    
