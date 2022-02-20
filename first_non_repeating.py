620d5c3b7333c9c0ad5c30437cd0ef0a80f543b1
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-20 18:06:07 +0300

    task was written in the file

---
+++ first_non_repeating.py
@@ -0,0 +1,7 @@
+    #TASK
+
+#   Write a function named first_non_repeating_letter that takes a string input, and returns the first character that is not repeated anywhere in the string. For Example, if given the input 'stress', the function should retun 't'i since the letter t only occurs once in the string, and occurs first in the string. As an added challange, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'. If a string contains all repeating characters, it should return an empty string("") or None -- see sample tests. 
+
+    #SOLUTION1
+
+
