f3d2385175c78e56bf51198d40c5e680c34ec095
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-20 18:15:40 +0300

    task was written in the file

---
+++ rock_paper_scissor.py
@@ -0,0 +1,13 @@
+    #TASK
+
+#   Abigail and Benson are playing Rock, Paper, Scissors, Each game is represented by an array of length 2, where the first element represents what Abigail played and the second element represents what Benson played. Given a squence of game, determine who wins the most number of matches. If they tie, output "Tie". 
+    # R stands for Rock
+    #P stands for Paper
+    #S stands for Scissors 
+    # """
+
+#   x = [["R", "P"], ["R", "S"], ["S", "P"]]
+
+    #SOLUTION
+
+    
