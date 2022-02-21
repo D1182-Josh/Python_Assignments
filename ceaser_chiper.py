ca36e99f04561c8250c06dd52372d6bca80e6c25
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-21 18:35:47 +0300

    task was written in the file

---
+++ ceaser_chiper.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#   ROT13 is a simple letter substitution chiper that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an wxample of the Ceaser cipher. Create a function that takes a string and retuns the string ciphered with Rot13. If there are numbers or special characters included in the string, they shoukd be returned as tehey are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation". please note that using encode is considered cheating. 
+
+    ("tast") >>>> ("grfg")
+    ("Test") >>>> ("Grfg")
+    ("Tes12t // tESt") >>>> ("Grf12g // gRFg")
+
+    #SOLUTION
+
+    
