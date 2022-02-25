e48889aaf50e70118daa617edf337ad0919e6071
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-25 15:56:05 +0300

    task was written in the ascending_order.py file

---
+++ ascending_order.py
@@ -0,0 +1,13 @@
+    #TASK
+
+#Given a list of tuples, implement a methot that sorts the list in ascending order by the second element inside the tuples and retuns the list. 
+
+    Examples:
+
+        input: [('b', 1), ('c', 2), ('x', 3), ('x', 4), ('z', 0)]
+        output : [('z', 0), ('b', 1), ('c', 2), ('x', 3), ('x',4)]
+
+
+    #SOLUTION
+
+    
