ce930f6d390eb4a5c954b3f4ea7ebf20b3a00def
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-26 17:43:36 +0300

    task was written in the array_numbers.py file

---
+++ array_numbers.py
@@ -0,0 +1,9 @@
+    #TASK
+
+#Write an algorithm that takes an array and moves all the zeros t othe end, preserving the order of the other elements. 
+    sample = [1, 0, 1, 2, 0, 1, 3]
+    output = [1, 1, 2, 1, 3, 0, 0]
+
+  #SOLUTION1
+
+  
