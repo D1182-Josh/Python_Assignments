5bc109a67422084586e04281887a17b13255df56
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-26 17:52:07 +0300

    task was written in the sequence_of_fibonacci.py file

---
+++ sequence_of_fibonacci.py
@@ -0,0 +1,9 @@
+    #TASK
+
+#Write a python program that writes as many fibonacci sequences as we want from the user. 
+#Example; 1, 1, 1, 2, 3, 5, 8(first seven fibonacci numbers)
+
+
+    #solution
+
+    
