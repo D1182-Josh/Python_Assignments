18fdc1f79828c23081464d4fbc2aa76e2f63b080
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-26 17:45:53 +0300

    task and two solution were written in the array_numbers.py file

--- array_numbers.py
+++ array_numbers.py
@@ -6,4 +6,26 @@
 
   #SOLUTION1
 
+sample = [1, 0, 1, 2, 0, 1, 3]
+
+new_arry = [i for i in sample if i > 0]
+for i in range(sample.count(0)) :
+  new_arry.append(0)
+new_arry
+    
+    #SOLUTION2
+
+s1 = "hello"
+s2 = "world"
+
+for i in s1 :
+  for j in s2 :
+    if i == j :
+      print("YES")
+      break
+else:
+  print("NO")
+
+  
+
   
