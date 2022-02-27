8f0cf6cc1210da8ccfa20b7d25c701ad434a2d90
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-27 23:14:01 +0300

    task and solutions were added in thr product_discount.py file

--- product_discount.py
+++ product_discount.py
@@ -5,14 +5,44 @@
  Product B $23.50
  Product C $10.75
 She gets the cheapest one for free, so she ends up paying $15.99 + $23.50 = $39.49 , but what her receipt says is: 
- Product A $15.99 - Special discount = $12.57
- Product B $23.50 - Special discount = $18.47
- Product C $10.75 - Special discount = $8.45
- Total : $39.49
- #Create a function that takes is a list of prices for a customer's shopping cart and returns the prices with the discount applied. Round all prices to the cent. 
+ Product A $15.99-special discount= $12.57
+ Product B $23.50-special discount=$18.47
+ Product C $10.75-special discount = $8.45
+ Total: $39.49
+ 
+#Create a function that takes in a list of prices for a customer's shopping cart and retuns the prices with the discount applied. Round all prices to the cent.
 
     #SOLUTION
 
-    
+store = [5.75, 14.99, 36.83, 12.15, 25.30, 5.75, 5.75, 5.75]
+sort_list = sorted(store)
+indirim_oranı = round((sum(sort_list[:len(store) // 3]) * 100 ) / sum(store) ,2)
+print(len(sort_list) // 3)
+
+sort_list
+indirim_oranı = round((sum(sort_list[: len(store) // 3]) * 100 ) / sum(store) , 2 )
+new_store = []
+for i in store :
+  new_store.append(round(i - ((i * indirim_oranı) / 100),2))
+
+new_store    
+
+
+    #SOLUTION
+
+[round(i,2) for i in list(map(lambda x : x - (x * indirim_oranı) / 100 , store))]
+[2.99, 5.75, 3.35, 4.99]
+
+store = [10.75, 11.68]
+sort_list = sorted(store)
+indirim_oranı = round((sum(sort_list[:len(store) // 3]) * 100 ) / sum(store) ,2)
+
+[round(i,2) for i in list(map(lambda x : x - (x * indirim_oranı) / 100 , store))]
+
+
+
+
+
+
 
 
