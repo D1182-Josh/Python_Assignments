dea711333d1989405e74477e3bd273c4887a822d
Coşkun DUMAN <dumanc753@gmail.com>
2022-02-27 23:00:56 +0300

    TASK was writtne in the product_discount.py file

---
+++ product_discount.py
@@ -0,0 +1,18 @@
+    #TASK
+
+ #Aretailer is having a store-wide "buy 2, get 1 free" sale. For legal reasons, they can't charge their customer $0 for an article so a discount is applied to all products instead. For example, if a customer gets three products a , b and c: 
+ Product A $15.99
+ Product B $23.50
+ Product C $10.75
+She gets the cheapest one for free, so she ends up paying $15.99 + $23.50 = $39.49 , but what her receipt says is: 
+ Product A $15.99 - Special discount = $12.57
+ Product B $23.50 - Special discount = $18.47
+ Product C $10.75 - Special discount = $8.45
+ Total : $39.49
+ #Create a function that takes is a list of prices for a customer's shopping cart and returns the prices with the discount applied. Round all prices to the cent. 
+
+    #SOLUTION
+
+    
+
+
