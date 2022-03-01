a9aa6845f197bfd877e596baf269c4d4e912275a
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-01 22:29:30 +0300

    Task was written in the sorts_numbers.py file

---
+++ sorts_numbers.py
@@ -0,0 +1,13 @@
+    #TASK
+
+#Write a function that sorts the positive numbers in ascending order, and keeps the negative numbers untouched. 
+    Examples 
+    pos_neg_sort([6, 3, -2, 5, -8, 2, -2]) --> [2, 3, -2, 5, -8, 6, -2]
+    pos_neg_sort([6, 5, 4, -1, 3, 2, -1, 1]) --> [1, 2, 3, -1, 4, 5, -1, 6]
+    pos_neg_sort([-5, -5, -5, -5, 7, -5]) --> [-5, -5, -5, -5, 7, -5]
+    pos_neg_sort([]) --> []
+
+
+    #SOLUTION
+
+    
