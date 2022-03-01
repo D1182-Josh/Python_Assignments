2ea037ac7e720e26a400845b1981c1042fcbb198
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-01 22:07:13 +0300

    Task was written in the pyramid_with_nums.py file

---
+++ pyramid_with_nums.py
@@ -0,0 +1,13 @@
+    #TASK
+#Write a function like this
+
+
+                    * 1
+                  2 1 * *
+                * * * 1 2 3
+              4 3 2 1 * * * * 
+
+
+    #SOLUTION
+
+    
