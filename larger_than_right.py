19835c420315307c3700d1ce3af9c2c47ff1fa87
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-02 14:57:12 +0300

    name was changed as larger_than_right.py file

--- larger_than_right.py
+++ larger_than_right.py
@@ -11,6 +11,19 @@
     The last number in an array is trivially strictly  larger than all numbers that follow it (no numbers follow it).
 
 
-    #SOLUTION
+    #SOLUTION1
+
+arry = [3, 13, 11, 2, 1, 9, 5]
+# output [13, 11, 9, 5]
+n_arry = []
+for i in  range(0,len(arry)-1) :
+  if all([arry[i] > j for j in arry[i+1:]]) == True :
+    n_arry += [arry[i]]
+
+n_arry + [arry[-1]]
+
+    #SOLUTION2
+
+    
 
     
