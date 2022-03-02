0079808b95579dec7d4609e50d85b05181e8cbb5
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-02 14:55:03 +0300

    Task was written in the larger_than_right.py file

---
+++ larger_than_right.py
@@ -0,0 +1,16 @@
+    #TASK
+
+# Create a function that retrieves every number that is strictly larger than every nymber that follows it. 
+    Examples :
+        larger_than_right([3, 13, 11, 2, 1, 9, 5]) --> [13, 11, 9, 5]
+        13 is larger than all numbers to its right, etc.
+    Must be strictly larger. Always include the last number. 
+    larger_than_right([5, 5, 5, 5, 5,5]) --> [5]
+    larger_than_right([5, 9, 8, 7]) --> [9, 8, 7]
+    Notes
+    The last number in an array is trivially strictly  larger than all numbers that follow it (no numbers follow it).
+
+
+    #SOLUTION
+
+    
