209fa8cc73fda9dd5dbb2997238ca58f00643ea9
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-05 18:22:06 +0300

    task was written in the reverse_vowels.py file

---
+++ reverse_vowels.py
@@ -0,0 +1,14 @@
+    #TASK
+
+#Given a string s, reverse only all the vowels in the string and return it. The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in the both cases. 
+    Example 1:
+        Input: s= "hello"
+        Output: "holle"
+
+    Example 2:
+        Input: s = "leetcode"
+        Output: "leotcede"
+
+    #SOLUTION
+
+    
