f674ef7f24ce1f300eba67fa5d387a077c6a2ded
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-08 20:14:51 +0300

    task was written in the character_frequency.py file

---
+++ character_frequency.py
@@ -0,0 +1,7 @@
+    #   TASK
+
+#Write a Python program to count the number of characters in a string. Sample String: 'google.com' Expected Result : {'g' : 2, 'o' : 3, 'l' : 1, 'e' : 1, '.' : 1, 'c' : 1, 'm' : 1}
+
+    #SOLUTION
+
+    
