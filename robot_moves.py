fb1d3bd1f60594648f9686522fd9c3759c388691
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-08 20:46:05 +0300

    task and solution were written in the robot_moves.py file

--- robot_moves.py
+++ robot_moves.py
@@ -8,4 +8,12 @@
 
     #SOLUTION
 
-    
+moves = "LLLUUD"
+x,y = 0 , 0
+for i in moves :
+  if i == "L" : x += 1
+  elif i == "R" : x -= 1
+  elif i == "D" : y -= 1
+  elif i == "U" : y += 1 
+
+print(True if x == 0 and y == 0 else False)     
