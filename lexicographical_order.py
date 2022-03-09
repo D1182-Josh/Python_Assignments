25f226216ccd14b012a8c9f328fe4af0018a654d
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-09 21:57:20 +0300

    task was written in the lexicographical_order.py file

---
+++ lexicographical_order.py
@@ -0,0 +1,24 @@
+    #TASK
+
+#   Given two arrays of string a1 and a2 return a sorted array r in lexicographical order of strings of a1 which are substrings of strings of a2.
+
+    Example 1 :
+        a1 = ["arp", "live", "strong"]
+        a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+        returns["arp", "live", "strong"]
+
+    Example 2:
+        a1 = ["tarp", "mice", "bull"]
+        a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+        return[]
+
+    Notes:
+#        Arrays are written in "general" notation. See "Your Test Cases" for examples in your language. In Shell bash a1 and a2 are string. The return is a string where words are seperated by commas. Baware: r must be without duplicates. 
+
+    #SOLUTION1
+
+    
+
+    
+
+
