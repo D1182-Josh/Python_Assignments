e8acd80e5b697c88a529022d1023d2bf14d78393
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-12 22:04:28 +0300

    task was written in the convert_uppercase.py file

---
+++ convert_uppercase.py
@@ -0,0 +1,7 @@
+    #TASK
+
+#   Write a Python function to convert a given string to all uppercase if it contains at least 2 uppercase characters in the first 4 characters. 
+
+    #SOLUTION
+
+    
