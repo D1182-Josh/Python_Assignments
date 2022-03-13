d588598525956fb42ce673e180241a4f3e8f5785
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-13 17:08:28 +0300

    task was written in the world_landmass.py file

---
+++ world_landmass.py
@@ -0,0 +1,19 @@
+    #TASK
+
+#   Create a function that takes a country's name and its area as argument and returns the area of the country's proportion of the total world's landmass. 
+
+    Examples:
+        ("Russia", 17098242) --> "Russia is 11.48% of the world's landmass"
+
+        ("USA", 9372610) --> "USA is 6.29% of the world's landmass"
+
+        ("Iran", 1648195) --> "Iran is 1.11% of the world's landmass"
+
+    Notes:
+
+        The total world's landmass is 148,940,000[Km^2]
+        Round the result to two decimal places.
+
+    #SOLUTION
+
+    
