3c5aeea72e3f1d759922bf1d44317f4d9a73cea2
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-13 17:15:08 +0300

    task and solution were written in the number_convert_to_letter.py file

--- number_convert_to_letter.py
+++ number_convert_to_letter.py
@@ -4,4 +4,25 @@
 
     #SOLUTION
 
-    
+num = int(input("Enter your number: "))
+ones = ["sıfır", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"]
+tens = ["", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"]
+hundred = ["", "yüz", "iki yüz", "üç yüz", "dört yüz", "beş yüz", "altı yüz", "yedi yüz", "sekiz yüz", "dokuz yüz"]
+
+digit_hundred = (num % 1000 - num % 100) // 100
+digit_tens = (num % 100 -num % 10 ) // 10
+digit_ones = num % 10
+
+
+if len(str(num)) == 1 :
+  print(ones[digit_ones])
+elif len(str(num)) == 2 :
+  if str(num).endswith("0"):
+    print(tens[digit_tens])
+  else :
+    print(tens[digit_tens], ones[digit_ones])
+elif len(str(num)) == 3 :
+  if str(num).endswith("0") :
+    print(hundred[digit_hundred],tens[digit_tens])
+  else :
+     print(hundred[digit_hundred],tens[digit_tens], ones[digit_ones])    
