5caf4f8e4480540fe5d69d2d74f9f3e37ca85dee
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-18 01:04:12 +0300

    task was written in the no_duplicate_letters.py file

---
+++ no_duplicate_letters.py
@@ -0,0 +1,15 @@
+    #TASK
+
+#Given a common phrase, retun False if any individual word in the phrase contains duplicate letters. return True otherwise. 
+    Examples
+     no_duplicate_letters("Fortune favours the bold.") --> True
+    no_duplicate_letters("You can a horse to water, but you can't make him drink.") --> True
+     no_duplicate_letters("Look before you leap.") --> False
+     Duplicate letters in "Look " and "before". 
+      no_duplicate_letters("An apple a day keeps the doctor away.") --> False
+      Duplicate letters in "apple", ,"keeps", "doctor", and "away".
+
+
+     #SOLUTION
+
+     
