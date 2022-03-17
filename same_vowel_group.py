0940c4bc407227f2132cbaf10b79d45636baae19
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-18 00:52:53 +0300

    task was written in the same_vowel_group.py file

---
+++ same_vowel_group.py
@@ -0,0 +1,12 @@
+    #TASK
+
+#Write a function that select all words that have all  the same vowel (in any order and /or number) as the first word, indluding the first word.
+#Examples 
+    same_vowel_group(["toe", "ocelot", "maniac"]) --> ["toe", "ocelot"]
+    same_vowel_group(["many", "carriage", "emit", "apricot", "animal"]) --> ["many"
+
+#            Notes: Words will contain only lower case latter, and may contain whitespaces. Frequency does not matter ( e.g. if the first word is " loopy", then you can include words with any number of o's, so long as they only contain o's, and not other vowels).
+
+    #SOLUTION 
+
+    
