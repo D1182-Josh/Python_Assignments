ea6b4cc809b6a06a1a777eb3951ebd1bb56f3c53
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-18 00:54:01 +0300

    task and solution1  written in the same_vowel_group.py file

--- same_vowel_group.py
+++ same_vowel_group.py
@@ -7,6 +7,25 @@
 
 #            Notes: Words will contain only lower case latter, and may contain whitespaces. Frequency does not matter ( e.g. if the first word is " loopy", then you can include words with any number of o's, so long as they only contain o's, and not other vowels).
 
-    #SOLUTION 
+    #SOLUTION1
+
+words = ["hoops", "chuff", "bot", "bottom"]
+new_words = []
+for i in words :
+  for j in i :
+    if j not in "aeiou" :
+      i = i.replace(j, "")
+  new_words += [i]
+same = [words[0]]
+for i in range(1,len(new_words)) :
+  if set(new_words[0]) == set(new_words[i]) :
+    same += [words[i]]
+
+print(same)
+
+
+        #SOLUTION2
+    
+        
 
     
