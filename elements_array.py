c767d1bd4b01bd97f5969315ed866e9370ea2a81
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-21 20:37:27 +0300

    task was written in the  elements_array.py file

---
+++ elements_array.py
@@ -0,0 +1,13 @@
+    #TASK
+
+#In this challenge you will be given an array similar to the following : [[3], 4, [2], [5], 1, 6] in words, elements of the array are either an integer or an integer containing a single integer.We humans can clearly see that this array can reasonably be sorted according to "the content of the elements" as : [1, [2], [3], 4, [5], 6] Create a function that, given an array similar to the above , sorts the array according to the " content of the elements". 
+    Examples:
+         sort([4, 1, 3]) --> [1, 3, 4]
+         sort([[4], [1], [3]]) --> [[1], [3], [4]]
+
+
+    #SOLUTION1
+
+
+
+    
