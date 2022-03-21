bcc1af66eee76bfeb785ba747e73b0c497abe20e
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-21 20:56:21 +0300

    task was written in the clone_array.py file

---
+++ clone_array.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#The Code tab has a code which attempts to  add a clone of an array to itself. There is no error message, but the results are not as expected. Can you fix the code_
+    Examples:
+         clone([1, 1]) --> [1, 1, [1, 1]]
+         clone([1, 2, 3]) --> [1, 2, 3, [1, 2, 3]]
+         clone (["x", "y"]) --> ["x", "y"]]
+
+     #SOLUTION
+
+     
