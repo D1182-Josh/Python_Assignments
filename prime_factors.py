26a62c2a5df5011dcae1c5a621ee225716a3007e
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-23 21:58:55 +0300

    task was written in the  prime_factor.py file

---
+++ prime_factors.py
@@ -0,0 +1,11 @@
+    #TASK
+
+#create a function that returns a list containing the prime factor of whatever integer is passed to it. 
+Examples:
+
+    prime_factors(20) --> [2, 2, 5]
+    prime_factors(100) --> [2, 2, 5, 5]
+
+    #SOLUTION1
+
+    
