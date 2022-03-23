7af79f355defbddd2b08bdaae38300bb3d9edd34
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-23 22:05:28 +0300

    solution2  was written in the  prime_factor.py file

--- prime_factors.py
+++ prime_factors.py
@@ -28,7 +28,29 @@ prime_factor
 ... [2, 5, 2, 5] 
 
     #SOLUTION2
+def prime_factors(number) :
+  primes = []
+  prime_factor = []
+  for num in range(2, number + 1):
+    for i in range(2, num):
+      if (num % i) == 0:
+        break
+    else:
+      primes += [num]
 
+  while number != 1 :
+    for j in primes :
+      if number % j == 0 :
+        prime_factor += [j]
+        number = number / j
+  return sorted(prime_factor)
+
+prime_factors(111)
+... [3, 37]
+
+        #SOLUTION3
+
+        
 
 
 
