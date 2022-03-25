3d01d7ecf49add4192ae9cf2826b65d119d5c6f3
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-25 11:54:00 +0300

    task was written in the ceaser_cipher2.py file

---
+++ ceaser_cipher2.py
@@ -0,0 +1,7 @@
+    #TASK
+
+# ACeaser cipher is a simple substitution cipher in which each letter of the plain text is substituted with a leeeter found by moving n places down the alphabet. For example, assume the input plain text is the following : "abcdxyz" if the shift value, n, is 4, then the encryted  text would be the following: "efghbcd" You are to write a function that accepts two arguments, a plain-text message and a number of letters to shift in the cipher. The function will return an encrypted string with all letters transformed and all punctuation and whitespace remaining unchanged. Note: You can assume the plain text is all lowercase ASCII except for whitespace and punctuation.
+
+    #SOLUTION
+
+    
