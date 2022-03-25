dbcd18d367cd9779df7222e012ba30ae91ff972f
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-25 11:45:49 +0300

    task was written in the removeNb.py file

---
+++ removeNb.py
@@ -0,0 +1,21 @@
+    #TASK
+# A friend of mine takes the squence of all numbers from 1 to n (where n > 0). Within taht squence, he chooses two numbers, a and b. He says taht the product of a and b should be equal to the sum of all numbers in the squence, excluding a and b. Given a number n, could you tell me the numbers he excluded from the squence? The function takes the parameter: n ( n is always strictly greater than 0) and returns an array or a string (depending on the language) of the form:
+    [(a,b, ...] or [[a,b], ...] or {{a,b}, ...} or [{a,b}, ...]
+#with all (a,b) which are the possible removed numbers in the squence 1 to n. [(a,b), ...] or  [(a,b, ...] or [[a,b], ...]  [(a,b, ...] or ....
+#It happens that rhere are several possible (a,b). The function returns an empty array (or an empty string) if no possible numbers are found which prove that my friend has not told the truth! 
+    Examples:
+        removeNb(26) should return [(15,21), (21,15)]
+        or 
+        removeNb(26) shoud return {{15,21}, {21,15}}
+        or 
+        removeNb(26) should return [[15,21],[21,15]]
+        or 
+        removeNb(26) should return [{15,21},{21,15}]
+        or 
+        removeNb(26) should return "15 21, 21 15"
+        in C:
+# removeNb(26) should return {{15,21}{21,15}} tested by way of string. Function removeNb should return a pointer to an allocatedarray of  Pair  pointer, each one also allocated.
+
+    #SOLUTION
+
+    
