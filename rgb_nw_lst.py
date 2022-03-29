1a389d4f67bc3cc16cac94cda879997aa5d79acf
Coşkun DUMAN <dumanc753@gmail.com>
2022-03-29 20:23:24 +0300

    task was written in the rgb_nw_lst.py file

---
+++ rgb_nw_lst.py
@@ -0,0 +1,8 @@
+    #TASK
+
+#   The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 – 255. Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+    Examples:
+
