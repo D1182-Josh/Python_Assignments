7af749916db37e21d69f8c399b2459cc6dfdb9f2
Coşkun DUMAN <dumanc753@gmail.com>
2022-04-01 23:15:08 +0300

    task was written in the rangoli_alphabet.py file

---
+++ rangoli_alphabet.py
@@ -0,0 +1,35 @@
+    #TASK
+                              Exercise 144 
+
+Alphabet Rangoli
+You are given an integer, . Your task is to print an alphabet rangoli of size . (Rangoli is a form of Indian folk art based on creation of patterns.)
+Different sizes of alphabet rangoli are shown below:
+
+Sample Input
+
+5
+
+
+Sample Output
+
+--------e--------
+
+------e-d-e------
+
+----e-d-c-d-e----
+
+--e-d-c-b-c-d-e--
+
+e-d-c-b-a-b-c-d-e
+
+--e-d-c-b-c-d-e--
+
+----e-d-c-d-e----
+
+------e-d-e------
+
+--------e--------
+
+        #SOLUTION
+
+        
