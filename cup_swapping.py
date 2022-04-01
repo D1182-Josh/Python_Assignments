a941544c3f067b058a8b4a8d82d7a85069da4a4d
Coşkun DUMAN <dumanc753@gmail.com>
2022-04-02 02:02:26 +0300

    task was written in the cup_swapping.py file

---
+++ cup_swapping.py
@@ -0,0 +1,30 @@
+    #TASK
+
+
+#Cup Swapping
+There are three cups on a table, at positions A, B, and C. At the start, there is a ball hidden under the cup at position B.
+
+However, I perform several swaps on the cups, which is notated as two letters. For example, if I swap the cups at positions A and B, I could notate this as AB or BA.
+
+Create a function that returns the letter position that the ball is at, once I finish swapping the cups. The swaps will be given to you as a list.
+
+
+Examples
+
+cup_swapping(["AB", "CA"]) ➞ "C"
+
+cup_swapping(["AC", "CA", "CA", "AC"]) ➞ "B"
+
+cup_swapping(["BA", "AC", "CA", "BC"]) ➞ "A"
+
+Notes
+
+A swap could be notated in two different ways, since both ways end up with the same outcome.
+
+All swaps will be notated as capital letters and will be valid.
+
+You cannot swap a cup with itself.
+
+        #SOLUTION
+
+        
