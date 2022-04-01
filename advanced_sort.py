75600f6c233f6922a667bf6da1ad4ad880daedfc
Coşkun DUMAN <dumanc753@gmail.com>
2022-04-01 23:07:08 +0300

    task and solution were  written in the advanced_sort.py file

--- advanced_sort.py
+++ advanced_sort.py
@@ -19,6 +19,19 @@ Notes
 The sublists should be returned in the order of each element's first appearance in the given list.    
 
     #SOLUTION
+def advanced_sort(arry) :
+  arry_dict= {}
+  for i in arry :
+    if i not in arry_dict :
+      arry_dict[i] = [i]
+    else :
+      arry_dict[i] += [i]
+  return [i for i in arry_dict.values()]
+
+advanced_sort([5, 4, 5, 5, 4, 3])
+
+... [[5,5,5], [4,4],[3]]
+
 
 
 
