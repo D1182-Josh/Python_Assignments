65557536addd365659c5ae6993fcd5bb90297038
Coşkun DUMAN <dumanc753@gmail.com>
2022-04-10 04:40:32 +0300

    task and solution were  written in the guessing_game.py file

--- guessing_game.py
+++ guessing_game.py
@@ -4,4 +4,23 @@
 #and indicate in the code that each user has the right to guess 5, deduct 1 right for each wrong guess     
     #SOLUTION
 
-    
+import random
+import time
+rastgele = random.randint(1,100)
+hak = 5
+while True:
+    kullanıcı = int(input("sayı giriniz:"))
+    if kullanıcı<rastgele:
+        time.sleep(2)
+        hak -= 1
+        print("sayıyı yükselt")
+    elif kullanıcı>rastgele:
+        time.sleep(2)
+        hak -= 1
+        print("sayıyı düşür")
+    else:
+        print("tbr tbr")
+        break
+    if hak == 0:
+        print("hakkınız bitti")
+        break    
