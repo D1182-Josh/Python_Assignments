abd35cf51b8d09113b19b0a4264ab9cc93eb4340
Coşkun DUMAN <dumanc753@gmail.com>
2022-04-10 04:40:04 +0300

    task was written in the guessing_game.py file

---
+++ guessing_game.py
@@ -0,0 +1,7 @@
+    #TASK                            
+
+#Write a pyhton code, which is a number guessing game that tries to find the number that will be randomly selected from the number sequence from 1 to 100.
+#and indicate in the code that each user has the right to guess 5, deduct 1 right for each wrong guess     
+    #SOLUTION
+
+    
