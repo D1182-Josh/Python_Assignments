2a5729c884c9ae2af88d8d4edcabf80ab838ae36
Coşkun DUMAN <dumanc753@gmail.com>
2022-04-12 23:11:36 +0300

    task was written in the bad_files.py file

---
+++ bad_files.py
@@ -0,0 +1,18 @@
+    #TASK
+
+                        
+Your computer might have been infected by a virus! Create a function that finds the viruses in files and removes them from your computer.
+Examplesi
+remove_virus("PC Files: spotifysetup.exe, virus.exe, dog.jpg") ➞ "PC Files: spotifysetup.exe, dog.jpg"
+
+remove_virus("PC Files: antivirus.exe, cat.pdf, lethalmalware.exe, dangerousvirus.exe ") ➞ "PC Files: antivirus.exe, cat.pdf"
+
+remove_virus("PC Files: notvirus.exe, funnycat.gif") ➞ "PC Files: notvirus.exe, funnycat.gif")
+Notes
+
+Bad files will contain "virus" or "malware", but "antivirus" and "notvirus" will not be viruses.
+Return "PC Files: Empty" if there are no files left on the computer.       
+
+    #SOLUTION
+
+    
