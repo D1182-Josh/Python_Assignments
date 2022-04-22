2ad3392b956693dcf59bfdd92f52a2a94f511d3c
Coşkun DUMAN <dumanc753@gmail.com>
2022-04-23 02:35:57 +0300

    task was written in the array_nums.py file

---
+++ array_nums.py
@@ -0,0 +1,8 @@
+    #TASK
+
+#Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+#You must write an algorithm that without using the nested loops  and without using the division operation.    
+
+    #SOLUTION
+
+    
